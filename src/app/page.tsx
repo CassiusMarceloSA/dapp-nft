083c259d206838068fba46d86cc5482f03f3df51
Cassius de Ávila <cassius_avila@sicredi.com.br>
2024-11-17 22:02:02 -0300

    feat: enhance hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 
 const TEAM_PICS = [
   "https://media.licdn.com/dms/image/v2/C4D03AQEtky8V4JWrYA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1571681049955?e=1737590400&v=beta&t=Nv0NOzqUYPo71C6OQuB8YYOAYESEtOeu4gPCiYZlxjU",
-  "https://lh3.googleusercontent.com/a/ACg8ocLGKPKk8fG7TPQ9WPA6nYLPyHYCm8TP7nWH9qjapP5Q-wB1Rwfkn3ng6GdDqRW4suLiMy10mrUcm-ZPiYlaJTz-UDsEhqu7=s288-c-no"
+  "https://lh3.googleusercontent.com/a/ACg8ocLGKPKk8fG7TPQ9WPA6nYLPyHYCm8TP7nWH9qjapP5Q-wB1Rwfkn3ng6GdDqRW4suLiMy10mrUcm-ZPiYlaJTz-UDsEhqu7=s288-c-no",
 ];
 
 const IndexPage = () => (
@@ -31,7 +31,7 @@ const Hero = () => (
       className="absolute top-0 w-full h-full bg-center bg-cover"
       style={{
         backgroundImage:
-          "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
+          "url('https://images.unsplash.com/photo-1698309970011-c557c380319f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZG9nJTIwYmVhY2h8ZW58MHx8MHx8fDA%3D')",
       }}
     >
       <span
@@ -44,12 +44,12 @@ const Hero = () => (
         <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
           <div className="pr-12">
             <h1 className="text-white font-semibold text-5xl">
-              Your story starts with us.
+              Welcome to Proto's Metaverse
             </h1>
             <p className="mt-4 text-lg text-gray-300">
-              This is a simple example of a Landing Page you can build using
-              Tailwind Starter Kit. It features multiple CSS components based on
-              the Tailwindcss design system.
+              This is a dog photo NFT prototype: Learn by building your own NFT.
+              Expand your Web3 skills with this hands-on project. Code available
+              on GitHub. Contact me to discuss collaboration opportunities.
             </p>
           </div>
         </div>
@@ -77,54 +77,72 @@ const Hero = () => (
   </div>
 );
 
+type CardProps = {
+  title: string;
+  description: string;
+  icon: string;
+  to: string;
+  size?: "compact" | "regular";
+  color: string;
+};
+
+const Card = ({ size = "regular", ...props }: CardProps) => {
+  const position =
+    size === "compact"
+      ? "lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center"
+      : "w-full md:w-4/12 px-4 text-center";
+
+  return (
+    <div className={position}>
+      <a
+        className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg  cursor-pointer hover:scale-105 transition-all"
+        href={props.to}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="px-4 py-5 flex-auto">
+          <div
+            className={`text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full ${props.color}`}
+          >
+            <img src={`/${props.icon}.svg`} alt={`${props.icon} icon`} />
+          </div>
+          <h6 className="text-xl font-semibold">{props.title}</h6>
+          <p className="mt-2 mb-4 text-gray-600">{props.description}</p>
+        </div>
+      </a>
+    </div>
+  );
+};
+
 const Services = () => (
   <section className="pb-20 bg-gray-300 -mt-24">
     <div className="container mx-auto px-4">
       <div className="flex flex-wrap">
-        <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
-          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-            <div className="px-4 py-5 flex-auto">
-              <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
-                <i className="fas fa-award"></i>
-              </div>
-              <h6 className="text-xl font-semibold">Awarded Agency</h6>
-              <p className="mt-2 mb-4 text-gray-600">
-                Divide details about your product or agency work into parts. A
-                paragraph describing a feature will be enough.
-              </p>
-            </div>
-          </div>
-        </div>
+        <Card
+          title="Let's Contribute"
+          description="Open source for all! Explore the GitHub repository and contribute to the project"
+          icon="github"
+          to="https://github.com/CassiusMarceloSA/dapp-nft"
+          size="compact"
+          color="bg-red-400"
+        />
 
-        <div className="w-full md:w-4/12 px-4 text-center">
-          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-            <div className="px-4 py-5 flex-auto">
-              <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400">
-                <i className="fas fa-retweet"></i>
-              </div>
-              <h6 className="text-xl font-semibold">Free Revisions</h6>
-              <p className="mt-2 mb-4 text-gray-600">
-                Keep you user engaged by providing meaningful information.
-                Remember that by this time, the user is curious.
-              </p>
-            </div>
-          </div>
-        </div>
+        <Card
+          title="Let's Talk"
+          description="Let's talk about your project and how I can help. Connect with me in LinkedIn"
+          icon="linkedin"
+          to="https://www.linkedin.com/in/cassius-de-avila/"
+          color="bg-blue-400"
+        />
 
-        <div className="pt-6 w-full md:w-4/12 px-4 text-center">
-          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-            <div className="px-4 py-5 flex-auto">
-              <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400">
-                <i className="fas fa-fingerprint"></i>
-              </div>
-              <h6 className="text-xl font-semibold">Verified Company</h6>
-              <p className="mt-2 mb-4 text-gray-600">
-                Write a few lines about each one. A paragraph describing a
-                feature will be enough. Keep you user engaged!
-              </p>
-            </div>
-          </div>
-        </div>
+        <Card
+          title="Let's be curious"
+          description="Experience the future now! Interact with the smart contract and see how it works"
+          icon="cpu"
+          to="https://testnet.snowtrace.io/address/0xf33b1b4Cb3afe064291d365534AB8822eE7BdD08/contract/43113/code"
+          size="compact"
+          color="bg-green-400"
+        />
       </div>
 
       <div className="flex flex-wrap items-center mt-32">
