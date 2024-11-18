0a6258006ee0b42ddf07a62bbccbfe28a4a6cb87
Cassius de Ávila <cassius.avila@edu.pucrs.br>
2024-11-18 00:04:49 -0300

    refactor: enhance card component using a modular aproach

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,6 +7,69 @@ type CardProps = {
   color: string;
 };
 
+type WithChildren = { children: React.ReactNode };
+
+const Wrapper = (props: WithChildren & Pick<CardProps, "size">) => {
+  const position =
+    props.size === "compact"
+      ? "lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center"
+      : "w-full md:w-4/12 px-4 text-center";
+
+  return <div className={position}>{props.children}</div>;
+};
+
+const Container = (props: WithChildren) => {
+  return (
+    <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+      {props.children}
+    </div>
+  );
+};
+const LinkContainer = (props: WithChildren & Pick<CardProps, "to">) => {
+  return (
+    <a
+      className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg  cursor-pointer hover:scale-105 transition-all"
+      href={props.to}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {props.children}
+    </a>
+  );
+};
+
+const Content = (props: WithChildren) => {
+  return <div className="px-4 py-5 flex-auto">{props.children} </div>;
+};
+
+const Icon = (props: Pick<CardProps, "icon" | "color">) => {
+  return (
+    <div
+      className={`text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full ${props.color}`}
+    >
+      <img src={`/${props.icon}.svg`} alt={`${props.icon} icon`} />
+    </div>
+  );
+};
+
+const Indicator = (props: Pick<CardProps, "color"> & WithChildren) => {
+  return (
+    <div
+      className={`text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full ${props.color}`}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+const Title = (props: WithChildren) => {
+  return <h6 className="text-xl font-semibold">{props.children}</h6>;
+};
+
+const Description = (props: WithChildren) => {
+  return <p className="mt-2 mb-4 text-gray-600">{props.children}</p>;
+};
+
 const Card = ({ size = "regular", ...props }: CardProps) => {
   const position =
     size === "compact"
@@ -35,4 +98,13 @@ const Card = ({ size = "regular", ...props }: CardProps) => {
   );
 };
 
-export default Card;
+export default {
+  Wrapper,
+  Container,
+  LinkContainer,
+  Content,
+  Icon,
+  Indicator,
+  Title,
+  Description,
+};
